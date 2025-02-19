5d43fc360733ff86f987f0eb2f90df25ee82b1d0
thebkblk <thebkblk@gmail.com>
2025-02-19 01:03:36 -0500

    testing

--- 4-ShellP2/dragon.c
+++ 4-ShellP2/dragon.c
@@ -1,6 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <libgen.h>   
+#include <unistd.h>   
+#include <string.h>
+
+#define PATH_MAX 4096
+#define DREXEL_DRAGON "dragon.txt"
+
 
 // EXTRA CREDIT - print the drexel dragon from the readme.md
 extern void print_dragon(){
-  // TODO implement 
+    char exe_path[PATH_MAX];
+    ssize_t len;
+
+    len = readlink("/proc/self/exe", exe_path, sizeof(exe_path) - 1);
+    exe_path[len] = '\0'; 
+
+    char *dir = dirname(exe_path);
+
+    strcat(dir, "/");
+    strcat(dir, DREXEL_DRAGON);
+
+
+    FILE *file = fopen(dir, "r");
+    if (file == NULL) {
+        printf("error: failed to open %s: %s\n", DREXEL_DRAGON, dir);
+        return;
+    }
+
+    char line[256];
+    while (fgets(line, sizeof(line), file) != NULL) {
+        printf("%s", line);
+    }
+    printf("\n");
+
+    fclose(file);
 }
--- 4-ShellP2/dshlib.c
+++ 4-ShellP2/dshlib.c
@@ -8,6 +8,12 @@
 #include <sys/wait.h>
 #include "dshlib.h"
 
+#include <errno.h>
+
+extern void print_dragon(); // dragon.c
+
+int last_rc = 0;
+
 /*
  * Implement your exec_local_cmd_loop function by building a loop that prompts the 
  * user for input.  Use the SH_PROMPT constant from dshlib.h and then
@@ -103,14 +109,87 @@ int free_cmd_buff(cmd_buff_t *cmd_buff)
 int clear_cmd_buff(cmd_buff_t *cmd_buff)
 {
     cmd_buff->argc = 0;
-    cmd_buff->_cmd_buffer[0] = '\0';
+
+    if (cmd_buff->_cmd_buffer) { 
+        memset(cmd_buff->_cmd_buffer, 0, SH_CMD_MAX); 
+    }
+
+    memset(cmd_buff->argv, 0, sizeof(cmd_buff->argv)); 
     return OK;
 }
 
 int build_cmd_buff(char *cmd_line, cmd_buff_t *cmd_buff)
 {
+    // set up
+    clear_cmd_buff(cmd_buff); 
+
     removeSpaces(cmd_line);
-    strcpy(cmd_buff->_cmd_buffer, cmd_line);
+    
+    strncpy(cmd_buff->_cmd_buffer, cmd_line, SH_CMD_MAX - 1); 
+    cmd_buff->_cmd_buffer[SH_CMD_MAX - 1] = '\0';
+
+
+    char *token = cmd_buff->_cmd_buffer; // Use the buffer directly
+    int argc = 0;
+    bool in_quotes_mode = false;
+    char *start = NULL; 
+
+    while (*token) {
+
+        if (isspace((char)*token)){ // if space
+            if (in_quotes_mode == false){
+                token++;
+                continue;
+            }
+        }
+
+
+        if (*token == '\"') { // if quote 
+            in_quotes_mode = !in_quotes_mode;
+            if (in_quotes_mode) {
+                start = token + 1;
+            } else {
+                *token = '\0';
+                if (argc < CMD_MAX) {
+                    cmd_buff->argv[argc++] = start;
+                }
+            }
+            token++;
+            continue;
+        }
+
+        start = token;
+        if (in_quotes_mode) { // true -> in quote mode
+            while (*token && *token != '\"') { 
+                token++;
+            }
+            if (*token == '\"') {
+                *token = '\0'; 
+                in_quotes_mode = false; 
+                token++; 
+            }
+        } else { // false -> in quote mode
+            while (*token && !isspace((char)*token)) { 
+                if (*token == '\"') {
+                    break; 
+                }
+                token++;
+            }
+            if (*token) {
+                *token = '\0'; 
+                token++; 
+            }
+        }
+        if (argc < CMD_MAX) {
+            cmd_buff->argv[argc++] = start; // Store the argument
+        }
+
+
+    } // end while 
+
+    cmd_buff->argc = argc;
+    cmd_buff->argv[argc] = NULL;
+
     return OK;
 }
 
@@ -122,6 +201,8 @@ Built_In_Cmds match_command(const char *input)
         return BI_CMD_DRAGON;
     } else if (strcmp(input, "cd") == 0) {
         return BI_CMD_CD;
+    } else if (strcmp(input, "rc") == 0) {
+        return BI_RC;
     } else {
         return BI_NOT_BI;
     }
@@ -129,50 +210,137 @@ Built_In_Cmds match_command(const char *input)
 
 Built_In_Cmds exec_built_in_cmd(cmd_buff_t *cmd)
 {
-    if (strcmp(cmd->argv[0], "exit") == 0) {
-        return BI_EXECUTED;
-    } else if (strcmp(cmd->argv[0], "dragon") == 0) {
-        return BI_EXECUTED;
-    } else if (strcmp(cmd->argv[0], "cd") == 0) {
-        return BI_EXECUTED;
+    Built_In_Cmds cmd_type = match_command(cmd->argv[0]);
+    int rc = BI_NOT_BI;
+    
+    if (cmd_type == BI_CMD_EXIT) {
+        rc = BI_CMD_EXIT;
+    } else if (cmd_type == BI_CMD_DRAGON) {
+        print_dragon();
+        rc = BI_EXECUTED;
+    } else if (cmd_type == BI_CMD_CD) {
+        if (cmd->argc == 1) {
+            // nothing happens
+            rc = BI_EXECUTED;
+        } else if (cmd->argc == 2) { 
+            if (chdir(cmd->argv[1]) != 0) {
+                printf("error: could not change directory to %s\n", cmd->argv[1]); 
+            }
+            rc = BI_EXECUTED;
+        } else { 
+            printf("error: too many arguments for cd\n"); 
+            rc = BI_EXECUTED;
+        }
+    } else if (cmd_type == BI_RC) {
+        printf("%d\n", last_rc); 
+        rc = BI_EXECUTED;
     } else {
-        return BI_RC;
+        rc = BI_NOT_BI;
     }
+
+    return rc;
 }
 
 int exec_cmd(cmd_buff_t *cmd)
 {
+    int f_result, c_result;
+    
+    f_result = fork();
+    if (f_result < 0) {
+        perror("fork error");
+        return ERR_EXEC_CMD; 
+    }
+    
+    if (f_result == 0) {
+        // The child will now exec, basically shape shifting itself
+        int rc = execvp(cmd->argv[0], cmd->argv);
+        if (rc < 0){
+            perror("fork error"); 
+            exit(errno);     
+        }
+    } else {
+        // This will be where the parent picks up
+        waitpid(f_result, &c_result, 0);
+
+        if (WIFEXITED(c_result)) {
+            int rc = WEXITSTATUS(c_result);
+
+            if (rc==0){
+                return OK;
+            } else if (rc==ENOENT) {
+                printf("Error: file not found\n");
+                return rc;
+            } else if (rc==EACCES) {
+                printf("Error: permission denied\n");
+                return rc;
+            } else if (rc==ENOEXEC) {
+                printf("Error: executable format error\n");
+                return rc;
+            } else if (rc==EIO) {
+                printf("Error: input/output error\n");
+                return rc;
+            } else {
+                printf("Command execution failed with code %d\n", rc);
+                return rc;
+            }
+        } else {
+            printf(CMD_ERR_EXECUTE);
+            return ERR_EXEC_CMD; 
+        }
+    }
+
     return OK;
 }
 
  int exec_local_cmd_loop()
 {
-    char *cmd_buff;
+    char *cmd_input = malloc(SH_CMD_MAX * sizeof(char));
     int rc = OK;
-    cmd_buff_t cmd;
+    cmd_buff_t cmd_buff;
+
+    // Allocate memory for cmd_buff
+    rc = alloc_cmd_buff(&cmd_buff);
+    if (rc == ERR_MEMORY) {
+        printf("Error allocating buffer\n");
+        return rc;
+    }
 
     while(1){
         printf("%s", SH_PROMPT);
-        if (fgets(cmd_buff, ARG_MAX, stdin) == NULL){
+        if (fgets(cmd_input, ARG_MAX, stdin) == NULL){
             printf("\n");
             break;
         }
         //remove the trailing \n from cmd_buff
-        cmd_buff[strcspn(cmd_buff,"\n")] = '\0';
+        cmd_input[strcspn(cmd_input,"\n")] = '\0';
+
+        rc = build_cmd_buff(cmd_input, &cmd_buff);
+
+        if (cmd_buff.argc == 0){
+            printf(CMD_WARN_NO_CMD);
+            continue;
+        }
+
+        rc = exec_built_in_cmd(&cmd_buff);
+
+        if(rc == BI_CMD_EXIT){
+            rc = OK_EXIT;
+            break;
+        }
+        if(rc == BI_EXECUTED){
+            rc = OK;
+            continue;
+        }
+        
+        rc = exec_cmd(&cmd_buff);
+
+        last_rc = rc;
+
      
-        //IMPLEMENT THE REST OF THE REQUIREMENTS
     }
 
+    free(cmd_input);
+    free_cmd_buff(&cmd_buff);
 
-    // TODO IMPLEMENT MAIN LOOP
-
-    // TODO IMPLEMENT parsing input to cmd_buff_t *cmd_buff
-
-    // TODO IMPLEMENT if built-in command, execute builtin logic for exit, cd (extra credit: dragon)
-    // the cd command should chdir to the provided directory; if no directory is provided, do nothing
-
-    // TODO IMPLEMENT if not built-in command, fork/exec as an external command
-    // for example, if the user input is "ls -l", you would fork/exec the command "ls" with the arg "-l"
-
-    return OK;
+    return rc;
 }
